e6fbc803b3a728f46b3376dd6a5e8ab5ffba2f94
hongcheol94 <macotong3@naver.com>
2022-07-28 15:01:35 +0900

    .

--- mysql/modeling/basic.sql
+++ mysql/modeling/basic.sql
@@ -1,1 +1,61 @@
 use momstouch;
+
+INSERT INTO infrInterest (
+ifinInterest
+,infrMember_ifmmSeq
+,infrCar_ifdlSeq
+)
+VALUES (
+"제네시스"
+,1
+,2
+),
+(
+"볼보"
+,2
+,1
+),
+(
+"캐딜락"
+,2
+,2
+)
+;
+
+    
+	
+    -- ALTER TABLE infrMember AUTO_INCREMENT = 1;
+    UPDATE infrCar
+    SET ifdlSeq = 3
+    where ifdlSeq = 15;
+	
+-- DELETE FROM infrMember WHERE ifmmSeq=25;
+
+
+
+
+
+
+
+
+	
+
+
+
+ 
+    
+
+    
+    
+	
+
+
+
+
+
+
+
+
+
+
+
